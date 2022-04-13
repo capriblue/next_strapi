7ebfd57164ecaedd51e0bef1c4604af954d327cf
capriblue <capri4wrk@gmail.com>
2022-04-13 17:30:15 +0900

    first commit

---
+++ components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+      <nav>
+          <a>Devistry</a>
+          <ul>
+              <li>
+                  <a>All posts</a>
+              </li>
+          </ul>
+      </nav>
+    
+  )
+}
